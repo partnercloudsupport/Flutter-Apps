a02d8cab1c6b8933b156c81d224faa6f636c59d6
thiagocarvalho0877 <tsc.carvalho@mat.ci.ufpb.br>
2018-09-22 10:50:34 -0300

    Added the new child of Column, Row, ant turn the FlatButtons into Row's children

--- contador_pessoas/lib/main.dart
+++ contador_pessoas/lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
       // I'm studying Column widget !
+      // Widget father: Column, Sons: Text, Row, Text
       home: new Column(
         // Children's Column
         children: <Widget>[
@@ -12,6 +13,27 @@ void main() {
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
               )),
+          // Widget father: Row, Sons: FlatButton, FlatButton
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text(
+                  "+1",
+                  style: TextStyle(color: Colors.white, fontSize: 40.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text(
+                  "-1", style: TextStyle(
+                  color: Colors.green, fontSize: 40.0, fontWeight: FontWeight.bold,
+                ),),
+                onPressed: () {},
+              ),
+            ],
+          ),
           Text("Pode entrar!", style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
