d300e119a6bdc21dc880aeb0e15c8854e78aa782
thiagocarvalho0877 <tsc.carvalho@mat.ci.ufpb.br>
2018-09-22 11:05:15 -0300

    Added Padding on Row and one of both FlatButtons to separate it

--- contador_pessoas/lib/main.dart
+++ contador_pessoas/lib/main.dart
@@ -4,8 +4,10 @@ void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
       // I'm studying Column widget !
-      // Widget father: Column, Sons: Text, Row, Text
+      // Widget father: Column, Sons: Text, Padding, Text
       home: new Column(
+        // Align at center the children's Column
+        mainAxisAlignment: MainAxisAlignment.center,
         // Children's Column
         children: <Widget>[
           Text("Pessoas",
@@ -13,26 +15,34 @@ void main() {
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
               )),
-          // Widget father: Row, Sons: FlatButton, FlatButton
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FlatButton(
-                child: Text(
-                  "+1",
-                  style: TextStyle(color: Colors.white, fontSize: 40.0,
-                      fontWeight: FontWeight.bold),
+          // Widget father: Padding, Son: Row
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+            // Widget father: Row, sons: Padding, FlatButton
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // Widget father: Padding, son: FlatButton
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(color: Colors.white, fontSize: 40.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {},
+                  ),
                 ),
-                onPressed: () {},
-              ),
-              FlatButton(
-                child: Text(
-                  "-1", style: TextStyle(
-                  color: Colors.green, fontSize: 40.0, fontWeight: FontWeight.bold,
-                ),),
-                onPressed: () {},
-              ),
-            ],
+                FlatButton(
+                  child: Text(
+                    "-1", style: TextStyle(
+                    color: Colors.green, fontSize: 40.0, fontWeight: FontWeight.bold,
+                  ),),
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
           Text("Pode entrar!", style: TextStyle(
             color: Colors.white,
