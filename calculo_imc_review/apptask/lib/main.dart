e63b32bbacdf60240a0a35f0c12f7250e53802f8
Thiago Carvalho <tsc.carvalho@mat.ci.ufpb.br>
2018-10-05 12:12:52 -0300

    Iniciado a revisão do app

---
+++ calculo_imc_review/apptask/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+// start of a project
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+// A Widget Stateful where it will be pass like argument to runApp -> home:
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    // Widget Scaffold
+    return Scaffold(
+      // Barra do app com texto
+      appBar: AppBar(
+          title: Text("Calculadora IMC")
+      )
+    );
+  }
+}
+
