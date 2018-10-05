8c8fe4e602b0228526d0c55ac8debc1d84edd73e
Thiago Carvalho <tsc.carvalho@mat.ci.ufpb.br>
2018-10-04 21:50:31 -0300

    App finalizado com todas as funcionalidades do curso. Adicionado o conceito e o widget StateFul e o uso de variaveis para manipulaçao dos widgets na tela. Adicionado a função _changePeople que faz a atualização da tela através de um setState que atualiza a tela

--- contador_pessoas/lib/main.dart
+++ contador_pessoas/lib/main.dart
@@ -17,54 +17,100 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  // variable counter of people
+  int _people = 0;
+  String _textPeople = "Pode entrar!";
+
+  // function that change the number of people 
+  void _changePeople(int delta) {
+    // delta is +1 or 1
+    // we need to use setState for update the screen after a change 
+    setState(() {
+          _people += delta;
+
+          if(_people < 0 ) {
+            _textPeople = "Mundo invertido!?";
+          } else if ( _people > 15){
+            _textPeople = "Lotado!";
+          } else {
+            _textPeople = "Pode entrar!";
+          } 
+
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // Align at center the children's Column
-      mainAxisAlignment: MainAxisAlignment.center,
-      // Children's Column
+    return Stack(
       children: <Widget>[
-        Text("Pessoas",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            )),
-        // Widget father: Padding, Son: Row
-        Padding(
-          padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-          // Widget father: Row, sons: Padding, FlatButton
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              // Widget father: Padding, son: FlatButton
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: FlatButton(
-                  child: Text(
-                    "+1",
-                    style: TextStyle(color: Colors.white, fontSize: 40.0,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  onPressed: () {},
-                ),
-              ),
-              FlatButton(
-                child: Text(
-                  "-1", style: TextStyle(
-                  color: Colors.green, fontSize: 40.0, fontWeight: FontWeight.bold,
-                ),),
-                onPressed: () {},
-              ),
-            ],
-          ),
+        Image.asset("images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
         ),
-        Text("Pode entrar!", style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontStyle: FontStyle.italic
-        ))
+        Column(
+          // Align at center the children's Column
+          mainAxisAlignment: MainAxisAlignment.center,
+          // Children's Column
+          children: <Widget>[
+            Text("Pessoas: $_people",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                )),
+            // Widget father: Padding, Son: Row
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              // Widget father: Row, sons: Padding, FlatButton
+              child: Row(
+                // Align at center the children's Row
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Widget father: Padding, son: FlatButton
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: () {
+                        // The button will sum 1 to people
+                        _changePeople(1);
+                      },
+                    ),
+                  ),
+                  // Widget father: Padding, son: FlatButton
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      // the button will sum -1 to people
+                      onPressed: () {
+                        _changePeople(-1);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(_textPeople,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic))
+          ],
+        )
       ],
     );
   }
 }
-
