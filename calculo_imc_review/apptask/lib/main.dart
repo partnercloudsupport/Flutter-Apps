798c043d8f1ce1baf499bb166623fdbd229ec32d
Thiago Carvalho <tsc.carvalho@mat.ci.ufpb.br>
2018-10-05 15:34:04 -0300

    Pre-mudança de cores

--- calculo_imc_review/apptask/lib/main.dart
+++ calculo_imc_review/apptask/lib/main.dart
@@ -39,9 +39,41 @@ class _HomeState extends State<Home> {
     ),
     body: Column(
       // Alinha os filhos da coluna no centro (não entendi pq stretch)
+      // O uso do stretch é para ele alinhar o eixo cruzado
+      // como o eixo principal da coluna é o vertical, o stretch vai alinhar
+      // no eixo cruzado, o horizontal, ocupando todo o espaço horizontal da coluna
       crossAxisAlignment: CrossAxisAlignment.stretch,
+      // children: Icon, TextField
       children: <Widget>[
-      Icon(Icons.person, size: 240.0,)
+      Icon(Icons.person_outline, size: 140.0, color: Colors.lightBlue[800],),
+      // Caixa de texto
+      TextField(keyboardType: TextInputType.number,
+      textAlign: TextAlign.center,
+        // Esse style é para o texto digitado no campo
+        style: TextStyle( fontSize: 24.0, color: Colors.black),
+        // decoração do TextField
+        decoration: InputDecoration(labelText: "Peso (kg)",
+          labelStyle: TextStyle(color: Colors.black, fontSize: 24.0,
+            fontWeight: FontWeight.bold)
+        ),
+      ),
+      TextField(keyboardType: TextInputType.number,
+        textAlign: TextAlign.center,
+        // Esse style é para o texto digitado no campo
+        style: TextStyle( fontSize: 24.0, color: Colors.white),
+        // decoração do TextField
+        decoration: InputDecoration(labelText: "Altura (metros)",
+          labelStyle: TextStyle(color: Colors.black, fontSize: 24.0,
+            fontWeight: FontWeight.bold
+            )
+          ),
+        ),
+        RaisedButton(
+          color: Colors.lightBlue[800],
+          onPressed: () {},
+          child: Text("Calcular", style: TextStyle(fontSize: 22.0, color: Colors.black)
+          ,) 
+        )
     ],),
     );
   }
