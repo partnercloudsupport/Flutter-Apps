9dc9e0a38b698f5bb743736e522c4b45c5d0dddd
Thiago Carvalho <tsc.carvalho@mat.ci.ufpb.br>
2018-10-06 11:26:51 -0300

    Adicionado função getData() que obtém o map do response

--- conversor_moedas_review/lib/main.dart
+++ conversor_moedas_review/lib/main.dart
@@ -3,17 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 // bliblioteca que permite fazer requisições e não travar o programa enquato espera
 import 'dart:async';
+// biblioteca que permite converter a string para um json (um map)
+import 'dart:convert';
 
 // link de requisição da api de conversão de moedas
 const request = "https://api.hgbrasil.com/finance/?=format=json&key=60df7606";
 
-void main() async {
   // usamos o await para esperar os dados serem obtidos
   // para isso nossa função deve ser async!
-  http.Response response = await http.get(request);
-  // Requisição feita com sucesso
-  print(response.body);
-
+void main() async {
   runApp(MaterialApp(
     home: Container()));
+}
+
+Future<Map> getData() async {
+  // objeto que irá fazer requisição
+  http.Response response = await http.get(request);
+  // json.decode() converte a string do response.body para um Map
+  // retornamos o Map
+  return json.decode(response.body);
 }
