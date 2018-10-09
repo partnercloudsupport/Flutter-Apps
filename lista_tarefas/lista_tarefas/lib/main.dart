de70b64f2150d964c00b449e4f1bbdd06f6f7a58
Thiago Carvalho <tsc.carvalho@mat.ci.ufpb.br>
2018-10-08 21:55:37 -0300

    Adicionado GetFile para obter o arquivo e saveData para salvar os dados no arquivo

--- lista_tarefas/lista_tarefas/lib/main.dart
+++ lista_tarefas/lista_tarefas/lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,8 +16,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
   @override
-  Widget build(BuildContext context) {
-    return Container();
+
+  List _toDoList = [];
+
+  // Função que irá retornar o arquivo
+  Future<File> getFile() async {
+  // obtém o diretório do app, como o endereço não é retornado na hora
+  // usamos o await
+    final directory = await getApplicationDocumentsDirectory();
+  // retorna o arquivo data.json no diretório directory
+    return File("${directory.path}/data.json");
   }
+  // salva os dados
+  Future<File> saveData() async {
+    // converte a list para json
+    String data = json.encode(_toDoList);
+    // o arquivo file recebe o arquivo
+    final file = await getFile();
+    // escrevemos os dados (data) no arquivo (file)
+    return file.writeAsString(data);
+  }
+    Widget build(BuildContext context) {
+      return Container();
+    }
 }
