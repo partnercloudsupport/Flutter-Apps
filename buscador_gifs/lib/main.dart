7cea2e7543b3249f28d904742a172ce4e07357c0
Thiago Carvalho <tsc.carvalho@mat.ci.ufpb.br>
2018-10-10 18:39:27 -0300

    Iniciado App Buscador de Gifs

---
+++ buscador_gifs/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// biblioteca com as interfaces
+import 'package:buscador_gifs/ui/ui.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
