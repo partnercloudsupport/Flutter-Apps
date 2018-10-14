7e7465e96c018dee6f84ea13c56d7f0e5b81bcf7
Thiago Carvalho <tsc.carvalho@mat.ci.ufpb.br>
2018-10-13 22:02:02 -0300

    contact_page: add Imagem e TextField da tela de contatos.

--- agenda_contatos/lib/ui/contact_page.dart
+++ agenda_contatos/lib/ui/contact_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:agenda_contatos/contact_helper/contact_helper.dart';
+import 'dart:io';
 
 class ContactPage extends StatefulWidget {
 
@@ -11,6 +12,8 @@ class ContactPage extends StatefulWidget {
 
 class _ContactPageState extends State<ContactPage> {
   
+  bool _userEdited = false;  // indica se o usuário editou o contato
+
   Contact _editedContact;
 
   @override
@@ -27,19 +30,48 @@ class _ContactPageState extends State<ContactPage> {
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Scaffold(
-         appBar: AppBar(
+    return Scaffold(
+        appBar: AppBar(
            centerTitle: true,
            backgroundColor: Colors.blueAccent,
            title: Text(_editedContact.name ?? "Novo Contato"),
-         ),
-         floatingActionButton: FloatingActionButton(
-          onPressed: (){},
-          child: Icon(Icons.save,),
+        ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: (){},
+            child: Icon(Icons.save,
+            ),
           backgroundColor: Colors.blueAccent,
-         ),
-       ),
+          ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(10.0),
+          child: Column(children: <Widget>[
+              Container(
+                height: 140.0,
+                width: 140.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    // caso a imagem do contato não seja null, ?, pegaremos o arquivo de imagem
+                    // caso seja null, : , usamos uma imagem padrão
+                    image: _editedContact.img != null ?
+                      FileImage(File(_editedContact.img)) :
+                        AssetImage("images/contact_image.png")
+                  )
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Nome"),
+                  onChanged: (text) {
+                    _userEdited = true;
+                    setState(() {
+                      _editedContact.name = text;
+                    });
+                  },
+              )
+            ],
+          )
+        ),
     );
   }
 }
