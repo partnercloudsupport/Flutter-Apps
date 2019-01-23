d71bcc4c7b6b086b592a318037fda94bbb431248
Thiago Carvalho <tsc.carvalho@mat.ci.ufpb.br>
2019-01-23 14:39:21 -0300

    Adicionado App da loja online. Até agora apenas comecei a home_tab e coloquei a funcionalidade de por imagens

---
+++ loja_online_aula/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:loja_online_aula/screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Loja Online',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141)
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(), // tela principal do app
+    );
+  }
+}
---
+++ loja_online_aula/lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:loja_online_aula/tabs/home_tab.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  final _pageController = PageController(); // controlador das telas
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView( // widget que permite navegar por telas
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(), // desabilita função de rolar pro lado a tela
+      children: <Widget>[
+        HomeTab()
+      ],
+    );
+  }
+}
