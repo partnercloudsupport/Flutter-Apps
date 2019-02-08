dd8d5df7d01030f13ad4392b7f8fdbaab4d60612
thiago <tsc.carvalho@mat.ci.ufpb.br>
2019-02-08 13:51:44 -0300

    Adicionado os StreamControllers para lidar com email e senha. Adicionado os getters para entrada e saida de dados das Streams

--- examples_stream/login_bloc/lib/screens/login_screen.dart
+++ examples_stream/login_bloc/lib/screens/login_screen.dart
@@ -6,9 +6,41 @@ class LoginScreen extends StatelessWidget{
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
-        children: <Widget>[  
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          SizedBox(height: 25.0,),
+          submitButton()
         ],
         ),
       );
   }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: "you@email.com",
+        labelText: "you@email.com"
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'password',
+        labelText: 'password'
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      color: Colors.blue,
+      child: Text('Login'),
+      onPressed: () {},
+    );
+  }
+
 }
